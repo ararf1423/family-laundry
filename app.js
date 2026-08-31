c1de72c717fa4c370030b42c582ed373840d5fea
ararf1423 <5n5zpmb2z9@privaterelay.appleid.com>
2026-08-31 22:47:10 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
- 
+const button = document.getElementById("laundryButton");
+const status = document.getElementById("status");
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  button.textContent = "送信中...";
+
+  try {
+    // 今はテスト用
+    status.textContent = "洗濯中";
+
+    button.textContent = "洗濯中";
+  } catch (error) {
+    console.error(error);
+
+    status.textContent = "エラーが発生しました";
+    button.textContent = "洗濯する";
+    button.disabled = false;
+  }
+});
